639969c4afc9fd43002b1f6354217af6020489c6
Manoj M <manm@BLR.ALLEGISINDIA.COM>
2023-02-24 10:47:37 +0530

    Boxing and Unboxing demo

---
+++ SimpleProject/src/main/demo/BoxingUnboxingDemo.java
@@ -0,0 +1,15 @@
+package main.demo;
+
+public class BoxingUnboxingDemo {
+
+	public static void main(String[] args) {
+		int a = 5;
+
+		Integer b = a; // Boxing
+
+		Integer c = new Integer(10);
+		
+		int d = c; //Unboxing
+	}
+
+}
