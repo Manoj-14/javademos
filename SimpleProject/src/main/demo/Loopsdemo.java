5d9af4ff094a38ed274fca84a8ec2303036f540a
Manoj M <manm@BLR.ALLEGISINDIA.COM>
2023-02-24 11:59:20 +0530

    for loop demo

---
+++ SimpleProject/src/main/demo/Loopsdemo.java
@@ -0,0 +1,22 @@
+package main.demo;
+
+public class Loopsdemo {
+	public static void main(String[] args) {
+//		for(int i=0 ; i< 4 ; i++) System.out.println("Hello World");
+//		
+//		
+//		for(int i = 1 ; i<11 ; i++) {
+//			if(i%2==0) System.out.println(i);
+//		}
+//		
+//		for(int i = 0 ; i<11 ; i+=2) {
+//			System.out.println(i);
+//		}
+		
+//		int sum = 0 ;
+//		for(int i =1 ; i<10 ; i+=2) sum+=i ;
+//		System.out.println(sum);
+		
+		for (int i = 10 ; i>0 ; i--) System.out.println(i);
+	}
+}
