15fe93ecde07cf6a175339785b1874c32dbdd04f
Manoj M <manumadhu1425@gmail.com>
2023-02-27 09:43:30 +0530

    Constructor

--- SimpleProject/src/main/demo/oops/inheritance/InheritanceDemo.java
+++ SimpleProject/src/main/demo/oops/inheritance/InheritanceDemo.java
@@ -2,10 +2,10 @@ package main.demo.oops.inheritance;
 
 public class InheritanceDemo {
 	public static void main(String[] args) {
-		Vehicals vehical = new Vehicals();
+		Vehicals vehical = new Vehicals(2,3);
 		vehical.fuel();
 		
-		Vehicals bus = new Bus();
+		Vehicals bus = new Bus(3,4,5);
 		bus.fuel();
 	}
 }
--- SimpleProject/src/main/demo/oops/inheritance/Vehicals.java
+++ SimpleProject/src/main/demo/oops/inheritance/Vehicals.java
@@ -1,13 +1,23 @@
 package main.demo.oops.inheritance;
 
 public class Vehicals {
-
+	int a , b ;
+	Vehicals(int a,int b){
+		this.a = a;
+		this.b = b ;
+	}
 	void fuel() {
 		System.out.println("Petrol");
 	}
 }
 
 class Bus extends Vehicals {
+	int c;
+	Bus(int a, int b,int c) {
+		super(a, b);
+		this.c = c;
+	}
+
 	void fuel() {
 		super.fuel();
 		System.out.println("CNG");
@@ -15,6 +25,11 @@ class Bus extends Vehicals {
 }
 
 class Car extends Vehicals {
+	Car(int a, int b) {
+		super(a, b);
+		// TODO Auto-generated constructor stub
+	}
+
 	void fuel() {
 		super.fuel();
 		System.out.println("Diesal");
@@ -22,6 +37,11 @@ class Car extends Vehicals {
 }
 
 class Bike extends Vehicals {
+	Bike(int a, int b) {
+		super(a, b);
+		// TODO Auto-generated constructor stub
+	}
+
 	void fuel() {
 		super.fuel();
 		System.out.println("Electrical");
