aad84ce72da0c94defe839e09686d78539d194a8
Manoj M <manumadhu1425@gmail.com>
2023-02-27 10:32:31 +0530

    abstraction

---
+++ SimpleProject/src/main/demo/abstractDemo/AbstractClassDemo.java
@@ -0,0 +1,33 @@
+package main.demo.abstractDemo;
+
+public class AbstractClassDemo {
+
+	public static void main(String[] args) {
+		BMWThreeSeries b3 = new BMWThreeSeries();
+		b3.start();
+		b3.accelarate();
+	}
+
+}
+
+abstract class BMW {
+	void start() {
+		System.out.println("BMW Started");
+	}
+
+	abstract void accelarate();
+}
+
+class BMWThreeSeries extends BMW {
+	@Override
+	void accelarate() {
+		System.out.println("Tree time accelarated");
+	}
+}
+
+class BMWFiveSeries extends BMW {
+	@Override
+	void accelarate() {
+		System.out.println("Five time accelarated");
+	}
+}
