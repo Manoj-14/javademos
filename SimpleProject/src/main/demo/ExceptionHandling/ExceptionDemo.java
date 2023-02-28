37aca75435344243d1909146850b858e4ad394f1
Manoj M <manumadhu1425@gmail.com>
2023-02-28 12:00:07 +0530

    Checked exception

--- SimpleProject/src/main/demo/ExceptionHandling/ExceptionDemo.java
+++ SimpleProject/src/main/demo/ExceptionHandling/ExceptionDemo.java
@@ -1,38 +1,51 @@
 package main.demo.ExceptionHandling;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionDemo {
 	public static void main(String[] args) {
 //		 nullpointerDemo();
-		zeroErrorDemo();
+//		zeroErrorDemo();
+		try {
+			checkedException();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
 	}
-	
-	
+
 	private static void nullpointerDemo() {
 		try {
-		String s=null;
-		System.out.println(s.length());
-		}catch(Exception e) {
+			String s = null;
+			System.out.println(s.length());
+		} catch (Exception e) {
 			System.out.println("Exception occured");
 		}
 	}
+
 	private static void zeroErrorDemo() {
-		int a,b,c,d;
+		int a, b, c, d;
 		System.out.print("Enter 2 numbers:");
 		Scanner input = new Scanner(System.in);
-		try {			
+		try {
 			a = input.nextInt();
 			b = input.nextInt();
 			int res = a / b;
-			System.out.printf("Result: %d\n",res);
-		}catch(ArithmeticException e){
+			System.out.printf("Result: %d\n", res);
+		} catch (ArithmeticException e) {
 			System.out.println("Arithmetic Exception");
-		}catch(InputMismatchException ime) {
+		} catch (InputMismatchException ime) {
 			System.out.println("Input Mismatch Exception");
 		}
-		
+
 //		System.out.println("Outside the try");
+	}
+
+	public static void checkedException() throws FileNotFoundException {
+
+		FileInputStream fs = new FileInputStream("C:\\Users\\anm\\git\\javademos\\SimpleProject\\src\\file.txt");
 	}
 }
