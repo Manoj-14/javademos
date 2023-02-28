87661e8559e88791096fe28fd197e499f7c74aab
Manoj M <manumadhu1425@gmail.com>
2023-02-28 12:42:32 +0530

    File input and output stream demo

---
+++ SimpleProject/src/main/demo/fileHandling/FileHandlingDemo.java
@@ -0,0 +1,72 @@
+package main.demo.fileHandling;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+public class FileHandlingDemo {
+//	Stream -> Logical reference to input source
+
+	/**
+	 * 1. Byte Stream -> Read and write Bytes 
+	 * 2. Char Stream -> Read and write 1 Char (1 Char = Bytes) 
+	 * 3. Buffered Stream -> Wrapper for Byte or Char Stream
+	 * 4. Object Stream -> Read and write all Objects
+	 **/
+
+	public static void main(String[] args) {
+		fileInputStream();
+		fileOutputStream();
+		fileInputStream();
+	}
+
+	
+
+	private static void fileInputStream() {
+		// TODO Auto-generated method stub
+		File file = new File("C:\\Users\\manm\\git\\javademos\\SimpleProject\\src\\file1");
+		int ch;
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			System.out.println("File Opened for read");
+//			System.out.println("-----------");
+			System.out.println("Data:");
+			while((ch =fis.read()) != -1) {
+				System.out.print((char)ch);
+			}
+			fis.close();
+			System.out.println("\n-----------");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void fileOutputStream() {
+		File file = new File("C:\\Users\\manm\\git\\javademos\\SimpleProject\\src\\file1");
+		try {
+			FileOutputStream fos = new FileOutputStream(file);
+			System.out.println("File Opened for write");
+//			System.out.println("-----------");
+			Random random = new Random();
+			int num = random.nextInt() % 10 ;
+			String data = "Writting from program updated " + num;
+			fos.write(data.getBytes());
+			System.out.println("File Writtend Successfully");
+			fos.close();
+			System.out.println("-----------");
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
