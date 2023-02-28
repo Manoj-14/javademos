85b7ec72a764f250396659e9efcf6b54d2781816
Manoj M <manumadhu1425@gmail.com>
2023-02-28 11:06:32 +0530

    Exception Handling demo

--- SimpleProject/src/main/demo/ExceptionHandling/ExceptionDemo.java
+++ SimpleProject/src/main/demo/ExceptionHandling/ExceptionDemo.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class ExceptionDemo {
 	public static void main(String[] args) {
+		 nullpointerDemo();
+	}
+	
+	
+	private static void nullpointerDemo() {
+		try {
+		String s=null;
+		System.out.println(s.length());
+		}catch(Exception e) {
+			System.out.println("Exception occured");
+		}
+	}
+	private static void zeroErrorDemo() {
 		int a,b,c,d;
 		System.out.print("Enter 2 numbers:");
 		Scanner input = new Scanner(System.in);
