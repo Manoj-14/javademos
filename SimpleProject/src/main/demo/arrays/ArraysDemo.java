4c5724f6d2d5cb7a62dfeb1845c462cc139b68af
Manoj M <manumadhu1425@gmail.com>
2023-03-01 11:28:23 +0530

    function to add 0 at 5 preceded with 3

--- SimpleProject/src/main/demo/arrays/ArraysDemo.java
+++ SimpleProject/src/main/demo/arrays/ArraysDemo.java
@@ -1,26 +1,40 @@
 package main.demo.arrays;
 
 public class ArraysDemo {
-	
-	
+
 	public static void main(String[] args) {
-		int arr[] = {10,20,30,40};
+		int arr[] = { 10, 20, 30, 40 };
 		int[] arr1;
-		
-		printArr(arr);
-		arr[0] = 13;
-		printArr(arr);
-		arr[3] = new Integer(34);
-		printArr(arr);
-		arr[4] = new Integer(37);
-		printArr(arr);
+
+//		printArr(arr);
+//		arr[0] = 13;
+//		printArr(arr);
+//		arr[3] = new Integer(34);
+//		printArr(arr);
+//		arr[4] = new Integer(37);
+//		printArr(arr);
+
+		addZeroAtFive();
 	}
-	
-	
+
 	public static void printArr(int[] arr) {
-		for(int i=0; i< arr.length ;i++) {
-			System.out.print(arr[i]+",");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + ",");
 		}
 		System.out.println();
+	}
+
+	public static void addZeroAtFive() {
+		int arr[] = { 3, 5, 4, 5, 6, 3, 5, 7 };
+		for (int i = 0; i < arr.length; i++) {
+			if (i == 0)
+				continue;
+			else 
+				if (arr[i - 1] == 3 && arr[i] == 5)
+					arr[i] = 0;
+
+		}
+
+		printArr(arr);
 	}
 }
