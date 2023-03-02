4fe576b77c81c31f8250b1294ee9e5d1ae050d6c
Manoj M <manumadhu1425@gmail.com>
2023-03-02 11:50:26 +0530

    Inner Classes Demo

---
+++ SimpleProject/src/main/demo/innerClasses/InnerClasss.java
@@ -0,0 +1,47 @@
+package main.demo.innerClasses;
+
+public class InnerClasss {
+	public static void main(String[] args) {
+		ElectronicDevices.powerOn();
+
+		ElectronicDevices.Chip.brandName();
+
+		ElectronicDevices.Chip chip = new ElectronicDevices.Chip();
+		chip.specs();
+		
+		ElectronicDevices laptop = new ElectronicDevices();
+		
+		
+		ElectronicDevices.Processor proc = laptop.new Processor();
+		proc.getVoltage();
+		
+		ElectronicDevices.Processor.processorBrand();
+		
+	}
+}
+
+class ElectronicDevices {
+	static void powerOn() {
+		System.out.println("Device powered on....");
+	}
+
+	static class Chip {
+		static void brandName() {
+			System.out.println("Brand name....");
+		}
+
+		void specs() {
+			System.out.println("Specifications of this chip....");
+		}
+	}
+
+	class Processor {
+		void getVoltage() {
+			System.out.println("Display voltage of the processor....");
+		}
+		
+		static void processorBrand() {
+			System.out.println("Processor Brand name....");
+		}
+	}
+}
