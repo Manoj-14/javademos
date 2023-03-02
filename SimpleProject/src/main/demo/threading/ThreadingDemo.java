138e7982283646bee7261ad58931d3cb03cd354e
Manoj M <manumadhu1425@gmail.com>
2023-03-02 22:59:14 +0530

    Threading Demo

---
+++ SimpleProject/src/main/demo/threading/ThreadingDemo.java
@@ -0,0 +1,28 @@
+package main.demo.threading;
+
+public class ThreadingDemo {
+	public static void main(String[] args) {
+		MultiThread mt = new MultiThread();
+		mt.start();
+		
+		System.out.println("------------");
+		
+		SingleThread st = new SingleThread();
+		st.printNumbers();
+		
+		
+	}
+}
+
+
+class SingleThread{
+	void printNumbers() {
+		for(int i=1 ; i<=200 ; i++) System.out.println(Thread.currentThread().getName()+"-"+i);
+	}
+}
+
+class MultiThread extends Thread{
+	public void run() {
+		for(int i=1 ; i<=200 ; i++) System.out.println(Thread.currentThread().getName()+"-"+i);
+	}
+}
