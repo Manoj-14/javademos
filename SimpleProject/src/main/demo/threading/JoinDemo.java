88bbab2f785c238b45a4d826bc48f38f323a83c1
Manoj M <manumadhu1425@gmail.com>
2023-03-03 10:07:11 +0530

    Find time time taken to execute

--- SimpleProject/src/main/demo/threading/JoinDemo.java
+++ SimpleProject/src/main/demo/threading/JoinDemo.java
@@ -8,7 +8,9 @@ public class JoinDemo {
 		System.out.println("Enter the number(Sum of N number):");
 		int n = input.nextInt();
 		
+		long startTime = System.currentTimeMillis();
 		Calculator calculator = new Calculator(n);
+		
 		calculator.start();
 		
 		try {
@@ -18,6 +20,9 @@ public class JoinDemo {
 		}
 		
 		System.out.println("Sum of first "+n+" is : "+calculator.getSum());
+		long endTime = System.currentTimeMillis();
+		System.out.println("Total time taken:"+(endTime-startTime)/1000+"Seconds");
+		input.close();
 	}
 }
 
@@ -33,6 +38,11 @@ class Calculator extends Thread{
 	
 	public void run() {
 		for(int i=0 ; i<=this.n ; i++) sum+=i;
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public int getSum() {
